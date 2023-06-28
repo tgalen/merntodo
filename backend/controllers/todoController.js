6b4aa18d4596984c68154d901ad3ba4adb246e2c
tgalen <tgalen84@gmail.com>
2023-06-28 08:45:24 -0400

    add async to controller functions

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,14 +1,14 @@
 // @desc Get Todos
 // @route GET /api/todos
 // @access PRIVATE
-const getTodos = (req, res) => {
+const getTodos = async (req, res) => {
   res.status(200).json({ message: "Get Todos" });
 };
 
 // @desc Create Todo
 // @route POST /api/todos
 // @access PRIVATE
-const createTodo = (req, res) => {
+const createTodo = async (req, res) => {
   if (!req.body.todoTitle) {
     res.status(400);
     throw new Error("Please add a Todo Title");
@@ -19,14 +19,14 @@ const createTodo = (req, res) => {
 // @desc Update Todo
 // @route PUT /api/todos/:id
 // @access PRIVATE
-const updateTodo = (req, res) => {
+const updateTodo = async (req, res) => {
   res.status(200).json({ message: `Update Todo ${req.params.id}` });
 };
 
 // @desc Delete Todo
 // @route DELETE /api/todos/:id
 // @access PRIVATE
-const deleteTodo = (req, res) => {
+const deleteTodo = async (req, res) => {
   res.status(200).json({ message: `Delete Todo ${req.params.id}` });
 };
 
