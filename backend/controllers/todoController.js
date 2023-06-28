839bd390d0ba2b3aa29fb3a9fdfdd6b84d3a13a2
tgalen <tgalen84@gmail.com>
2023-06-28 08:50:21 -0400

    add express async handler

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,34 +1,36 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get Todos
 // @route GET /api/todos
 // @access PRIVATE
-const getTodos = async (req, res) => {
+const getTodos = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Todos" });
-};
+});
 
 // @desc Create Todo
 // @route POST /api/todos
 // @access PRIVATE
-const createTodo = async (req, res) => {
+const createTodo = asyncHandler(async (req, res) => {
   if (!req.body.todoTitle) {
     res.status(400);
     throw new Error("Please add a Todo Title");
   }
   res.status(200).json({ message: "Create Todo" });
-};
+});
 
 // @desc Update Todo
 // @route PUT /api/todos/:id
 // @access PRIVATE
-const updateTodo = async (req, res) => {
+const updateTodo = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Todo ${req.params.id}` });
-};
+});
 
 // @desc Delete Todo
 // @route DELETE /api/todos/:id
 // @access PRIVATE
-const deleteTodo = async (req, res) => {
+const deleteTodo = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Todo ${req.params.id}` });
-};
+});
 
 module.exports = {
   getTodos,
