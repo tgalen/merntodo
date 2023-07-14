1ddf330e38290499f4679faf5ebc9efbd7405087
tgalen <tgalen84@gmail.com>
2023-07-14 12:00:35 -0400

    add todo schema

---
+++ backend/models/todoModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema(
+  {
+    todoTitle: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    type: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+    },
+    users: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      unique: true,
+    },
+    groups: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "group",
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("todo", todoSchema);
