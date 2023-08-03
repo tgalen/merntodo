1200d2925d2a28913d4444876d5cd04d11ecc6a8
tgalen <tgalen84@gmail.com>
2023-08-03 19:13:07 -0400

    add MUI Icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
