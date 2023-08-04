d19969785ffe162c807ed93a580b7004d731203f
tgalen <tgalen84@gmail.com>
2023-08-04 15:12:47 -0400

    add constants file

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,7 @@
+export const USERS_API = "http://localhost:5000/api/users";
+
+export const LOGIN_API = "http://localhost:5000/api/users/login";
+
+export const GET_ME_API = "http://localhost:5000/api/users/me";
+
+export const TODOS_API = "http://localhost:5000/api/todos";
