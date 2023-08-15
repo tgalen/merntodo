9190ff98b82a8bac4e2685afe19a8372a27b683f
tgalen <tgalen84@gmail.com>
2023-08-15 10:26:39 -0400

    style Navbar w/ search input and icons placeholder

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,12 +3,24 @@ import AppBar from "@mui/material/AppBar";
 import styled from "@emotion/styled";
 import Typography from "@mui/material/Typography";
 import FactCheckIcon from "@mui/icons-material/FactCheck";
+import Box from "@mui/material/Box";
 
 const Navbar = () => {
   const StyledToolbar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between",
   });
+
+  const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: "5px",
+    width: "40%",
+  }));
+
+  const Icons = styled(Box)(({ theme }) => ({
+    backgroundColor: "white",
+  }));
 
   return (
     <AppBar position="stick">
@@ -17,6 +29,8 @@ const Navbar = () => {
           tasq
         </Typography>
         <FactCheckIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>search</Search>
+        <Icons>icons</Icons>
       </StyledToolbar>
     </AppBar>
   );
