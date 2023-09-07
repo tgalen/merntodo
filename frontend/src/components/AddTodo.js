ce2bebe6d00900e1c2af23a8729acf6e3c513b49
tgalen <tgalen84@gmail.com>
2023-09-07 16:23:19 -0400

    add marginBottom to TextField in AddTodo

--- frontend/src/components/AddTodo.js
+++ frontend/src/components/AddTodo.js
@@ -5,12 +5,19 @@ import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import styled from "@emotion/styled";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
 
 const StyledModal = styled(Modal)({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  marginBottom: "10px",
 });
 
 const AddTodo = ({ loggedInTasqUser }) => {
@@ -41,9 +48,23 @@ const AddTodo = ({ loggedInTasqUser }) => {
             Add Todo
           </Typography>
           <Box>
-            <Typography variant="span" fontWeight={500}>
-              {loggedInTasqUser.userName}
-            </Typography>
+            <UserBox>
+              <Typography variant="span" fontWeight={500}>
+                {loggedInTasqUser.userName}
+              </Typography>
+            </UserBox>
+            <Box component="form" margin={1}>
+              <TextField
+                sx={{ width: "100%", marginBottom: "15px" }}
+                label="Todo Title"
+                variant="standard"
+              />
+              <TextField
+                sx={{ width: "100%", marginBottom: "15px" }}
+                label="Decription"
+                variant="standard"
+              />
+            </Box>
           </Box>
         </Box>
       </StyledModal>
