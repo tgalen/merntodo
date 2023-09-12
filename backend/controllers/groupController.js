656b39495ded8bbaeb7fbef8aac22d9360062ac4
tgalen <tgalen84@gmail.com>
2023-09-11 21:44:07 -0400

    add createGroup

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -10,6 +10,19 @@ const getGroups = asyncHandler(async (req, res) => {
   res.status(200).json(groups);
 });
 
+//@desc create Group
+// @route POST api/groups
+//@access PRIVATE
+const createGroup = asyncHandler(async (req, res) => {
+  const group = await Group.create({
+    groupName: req.body.groupName,
+    members: [req.user.id],
+    description: req.body.description,
+  });
+
+  res.status(200).json(group);
+});
+
 const inviteUserToGroup = asyncHandler(async (req, res) => {});
 
 const userLeaveGroup = asyncHandler(async (req, res) => {});
@@ -20,6 +33,7 @@ const deleteGroup = asyncHandler(async (req, res) => {});
 
 module.exports = {
   getGroups,
+  createGroup,
   inviteUserToGroup,
   userLeaveGroup,
   editGroup,
